e1fd856f56c5bda6c3fb4aa8627edd7b77b6f95d
goodf <goodf@DESKTOP-OCJ7R3O>
2016-05-02 16:18:26 -0700

    Started project and added MathService static class.

---
+++ PrimeCalculator/Services/MathService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeCalculator.Services
+{
+    public static class MathService
+    {
+        /// <summary>
+        ///     Will return an array of integers
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static int[] CalculatePrimes(int n)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
